83358e01621c0fe19e259b8cf7ef88eccbc554c4
Keyur Rathod <keyur.rathod84@gmail.com>
2022-01-31 19:05:06 +0530

    505 serve error

---
+++ clocking_system_frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import AddStaff from "./Components/AddStaff/AddStaff";
+import DateTime from "./Components/DateTime/DateTime";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello World !</div>
+      <DateTime />
+      <AddStaff />
+    </div>
+  );
+}
+
+export default App;
---
+++ clocking_system_frontend/src/Components/AddStaff/AddStaff.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+export const AddStaff = () => {
+  var [name, setName] = useState();
+
+  const nameUpdate = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    const postURL = "http://localhost:4000/api/staff/newData";
+    fetch(postURL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+      }),
+    }).then((res) => {
+      console.log(res);
+      alert("You have been added to the system!");
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Full Name:</label>
+        <input required onChange={nameUpdate}></input>
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddStaff;
